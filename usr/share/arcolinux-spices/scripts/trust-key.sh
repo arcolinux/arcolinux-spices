e805aa8775cda21d71075053c4d959650b48cd8a
Erik Dubois <erik.dubois@gmail.com>
2020-09-11 14:00:16 +0200

    2 extra keys added marco and john

--- usr/share/arcolinux-spices/scripts/trust-key.sh
+++ usr/share/arcolinux-spices/scripts/trust-key.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "Receiving, local signing and refreshing keys"
-sudo pacman-key -r 74F5DE85A506BF64
-sudo pacman-key --lsign-key 74F5DE85A506BF64
+#erik
+pacman-key -r 74F5DE85A506BF64
+pacman-key --lsign-key 74F5DE85A506BF64
+#marco
+pacman-key -r 74F5DE85A506BF64
+pacman-key --lsign-key F1ABB772CE9F7FC0
+#john
+pacman-key -r 74F5DE85A506BF64
+pacman-key --lsign-key 4B1B49F7186D8731
 sudo pacman-key --refresh-keys
 echo "###                 ArcoLinux key trusted                      ####"
