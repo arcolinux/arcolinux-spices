e5bbbd4ce0b1bb0eb274c8ddd4655a01372d85f5
Erik Dubois <erik.dubois@gmail.com>
2020-09-11 14:48:14 +0200

    wrong copy/paste

--- usr/share/arcolinux-spices/scripts/trust-key.sh
+++ usr/share/arcolinux-spices/scripts/trust-key.sh
@@ -4,10 +4,10 @@ echo "Receiving, local signing and refreshing keys"
 pacman-key -r 74F5DE85A506BF64
 pacman-key --lsign-key 74F5DE85A506BF64
 #marco
-pacman-key -r 74F5DE85A506BF64
+pacman-key -r F1ABB772CE9F7FC0
 pacman-key --lsign-key F1ABB772CE9F7FC0
 #john
-pacman-key -r 74F5DE85A506BF64
+pacman-key -r 4B1B49F7186D8731
 pacman-key --lsign-key 4B1B49F7186D8731
 sudo pacman-key --refresh-keys
 echo "###                 ArcoLinux key trusted                      ####"
