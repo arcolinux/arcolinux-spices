1db6599d57a35de6a3e9670df7aed9884c09d2fa
Erik Dubois <erik.dubois@gmail.com>
2020-09-16 13:40:47 +0200

    extra key from stephen

--- usr/share/arcolinux-spices/scripts/trust-key.sh
+++ usr/share/arcolinux-spices/scripts/trust-key.sh
@@ -9,5 +9,9 @@ pacman-key --lsign-key F1ABB772CE9F7FC0
 #john
 pacman-key -r 4B1B49F7186D8731
 pacman-key --lsign-key 4B1B49F7186D8731
+#stephen
+pacman-key -r 02D507C6EFB8CEAA
+pacman-key --lsign-key 02D507C6EFB8CEAA
+
 sudo pacman-key --refresh-keys
 echo "###                 ArcoLinux key trusted                      ####"
