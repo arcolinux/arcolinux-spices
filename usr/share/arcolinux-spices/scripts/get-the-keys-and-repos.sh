70492875cc1b1fb559e07a553dddbd666f3155aa
ArcoLinux <arcolinuxinfo@gmail.com>
2022-05-04 12:27:41 +0200

    update

--- usr/share/arcolinux-spices/scripts/get-the-keys-and-repos.sh
+++ usr/share/arcolinux-spices/scripts/get-the-keys-and-repos.sh
@@ -4,30 +4,39 @@
 
 sudo pacman -S wget --noconfirm --needed
 
-echo "Getting the ArcoLinux keys from the ArcoLinux repo"
-
+echo "Getting the ArcoLinux keys from the ArcoLinux repo - report if link is broken"
 sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-20230919-6-any.pkg.tar.zst -O /tmp/arcolinux-keyring-20230919-6-any.pkg.tar.zst
 sudo pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20230919-6-any.pkg.tar.zst
 
+echo "Getting the latest arcolinux mirrors file - report if link is broken"
+sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-22.04-01-any.pkg.tar.zst -O /tmp/arcolinux-mirrorlist-git-22.04-01-any.pkg.tar.zst
+sudo pacman -U --noconfirm --needed /tmp/arcolinux-mirrorlist-git-22.04-01-any.pkg.tar.zst
+	
 ######################################################################################################################
 
-echo "Getting the latest arcolinux mirrors file"
+if grep -q arcolinux_repo /etc/pacman.conf; then
 
-sudo wget https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist -O /etc/pacman.d/arcolinux-mirrorlist
-echo '
-#[arcolinux_repo_testing]
-#SigLevel = Required DatabaseOptional
-#Include = /etc/pacman.d/arcolinux-mirrorlist
+	echo "ArcoLinux repos are already in /etc/pacman.conf"
 
-[arcolinux_repo]
-SigLevel = Required DatabaseOptional
-Include = /etc/pacman.d/arcolinux-mirrorlist
+else
 
-[arcolinux_repo_3party]
-SigLevel = Required DatabaseOptional
-Include = /etc/pacman.d/arcolinux-mirrorlist
+	echo '
+	#[arcolinux_repo_testing]
+	#SigLevel = Required DatabaseOptional
+	#Include = /etc/pacman.d/arcolinux-mirrorlist
 
-[arcolinux_repo_xlarge]
-SigLevel = Required DatabaseOptional
-Include = /etc/pacman.d/arcolinux-mirrorlist' | sudo tee --append /etc/pacman.conf
-echo "DONE - UPDATE"
+	[arcolinux_repo]
+	SigLevel = Required DatabaseOptional
+	Include = /etc/pacman.d/arcolinux-mirrorlist
+
+	[arcolinux_repo_3party]
+	SigLevel = Required DatabaseOptional
+	Include = /etc/pacman.d/arcolinux-mirrorlist
+
+	[arcolinux_repo_xlarge]
+	SigLevel = Required DatabaseOptional
+	Include = /etc/pacman.d/arcolinux-mirrorlist' | sudo tee --append /etc/pacman.conf
+
+fi
+
+echo "DONE - UPDATE NOW"
