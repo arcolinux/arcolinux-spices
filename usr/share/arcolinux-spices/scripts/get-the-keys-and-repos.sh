1aedacc60c211f8ab31d71da8677ca6365b15171
arcolinuxz <arcolinuxinfo@gmail.com>
2022-12-05 06:20:32 +0100

    update

--- usr/share/arcolinux-spices/scripts/get-the-keys-and-repos.sh
+++ usr/share/arcolinux-spices/scripts/get-the-keys-and-repos.sh
@@ -9,8 +9,8 @@ sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-
 sudo pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20230919-6-any.pkg.tar.zst
 
 echo "Getting the latest arcolinux mirrors file - report if link is broken"
-sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-22.04-01-any.pkg.tar.zst -O /tmp/arcolinux-mirrorlist-git-22.04-01-any.pkg.tar.zst
-sudo pacman -U --noconfirm --needed /tmp/arcolinux-mirrorlist-git-22.04-01-any.pkg.tar.zst
+sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-22.12-01-any.pkg.tar.zst -O /tmp/arcolinux-mirrorlist-git-22.12-01-any.pkg.tar.zst
+sudo pacman -U --noconfirm --needed /tmp/arcolinux-mirrorlist-git-22.12-01-any.pkg.tar.zst
 	
 ######################################################################################################################
 
