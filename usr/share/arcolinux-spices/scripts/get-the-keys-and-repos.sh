5e8fbbdb9bd901b2e36438cebd65f79d23d06266
Erik Dubois <erik.dubois@gmail.com>
2022-12-10 13:13:52 +0100

    new keyring

--- usr/share/arcolinux-spices/scripts/get-the-keys-and-repos.sh
+++ usr/share/arcolinux-spices/scripts/get-the-keys-and-repos.sh
@@ -5,8 +5,8 @@
 sudo pacman -S wget --noconfirm --needed
 
 echo "Getting the ArcoLinux keys from the ArcoLinux repo - report if link is broken"
-sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-20251209-2-any.pkg.tar.zst -O /tmp/arcolinux-keyring-20251209-2-any.pkg.tar.zst
-sudo pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20251209-2-any.pkg.tar.zst
+sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-20251209-3-any.pkg.tar.zst -O /tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
+sudo pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
 
 echo "Getting the latest arcolinux mirrors file - report if link is broken"
 sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-22.12-01-any.pkg.tar.zst -O /tmp/arcolinux-mirrorlist-git-22.12-01-any.pkg.tar.zst
